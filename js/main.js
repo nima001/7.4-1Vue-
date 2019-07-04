387c3ae9d74d7c63b6d08a6900eff9d9a69dbd3a
nima001 <1994974597@qq.com>
2019-07-04 17:12:54 +0800

    添加了显示余额

--- js/main.js
+++ js/main.js
@@ -9,14 +9,48 @@ Vue.component(
     }
 );
 
+Vue.component(
+    'balance', {
+        template: '<div><show @show_money="showbalance"></show><div v-if="show">ji</div></div>',
+        methods:{
+            showbalance:function (data) {
+                this.show=true,
+                alert(data);
+                console.log("data",data);
+            }
+        },
+        data: function () {
+            return {
+                show:false,
+            }
+        }
+    }
+);
+
+Vue.component(
+    'show',{
+        template:'<button @click="showmoney">显示余额</button>',
+        props:['mon'],
+        methods:{
+            showmoney:function() {
+                this.$emit('show_money',{a:1,b:2});
+            }
+        }
+    }
+);
+new Vue({
+    el:'#app2',
+});
+
 var app = new Vue({
     el: '#app1',
     components: {
         'syq': {
             template: '<button @click="on_click">请点我</button>',
+            props:['message'],
             methods: {
                 on_click: function () {
-                    alert("haha");
+                    alert(this.message);
                 }
             }
         },
@@ -32,6 +66,10 @@ var app = new Vue({
                     return this.like_count++;
                 }
             }
+        },
+        'user':{
+            template:'<a :href="username">{{username}}</a>',
+            props:['username'],
         }
     }
 });
